8e76000da6e8293d72d464d6c9e4becaa7986d81
bala-amypo <bala@amypo.in>
2025-12-17 09:29:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -1,13 +1,24 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Studententity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -15,6 +26,7 @@ public class Studententity {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -22,6 +34,7 @@ public class Studententity {
     public String getEmail(){
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,35 +1,38 @@
-package com.example.demo.service.Impl;
+package com.example.demo.service.impl;
 
-import java.util.*;
-import org.springframework.stereotype.Service;
 import com.example.demo.entity.Studententity;
+import com.example.demo.repository.Studentrepository;
 import com.example.demo.service.Studentservice;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements Studentservice {
 
-    private final Map<Long, Studententity> store = new HashMap<>();
-    private long counter = 1;
+    private final Studentrepository studentrepository;
+
+    public StudentServiceImpl(Studentrepository studentrepository) {
+        this.studentrepository = studentrepository;
+    }
 
     @Override
     public Studententity insertStudententity(Studententity st) {
-        st.setId(counter++);
-        store.put(st.getId(), st);
-        return st;
+        return studentrepository.save(st);
     }
 
     @Override
     public List<Studententity> getAllStudententity() {
-        return new ArrayList<>(store.values());
+        return studentrepository.findAll();
     }
 
     @Override
     public Optional<Studententity> getOneStudent(Long id) {
-        return Optional.ofNullable(store.get(id));
+        return studentrepository.findById(id);
     }
 
     @Override
-    public void deleteStudent(Long id){
-        store.remove(id);
+    public void deleteStudent(Long id) {
+        studentrepository.deleteById(id);
     }
 }
